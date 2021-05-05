f091babd47df22fe98ed59eaf2e7152f97afa72e
Dario Cangialosi Arkenidar <dario.cangialosi@gmail.com>
2021-05-05 13:37:32 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,76 @@
+<script>
+import Tree from "./Tree.svelte"
+let test_tree1=[1,2,3,[4,5]]
+let test_tree2=[
+    [
+        "out1",
+        "fibonacci algorithm"
+    ],
+    [
+        "set",
+        "x",
+        0
+    ],
+    [
+        "set",
+        "y",
+        1
+    ],
+    [
+        "loop",
+        [
+            "expr",
+            [
+                "get",
+                "x"
+            ],
+            "<",
+            256
+        ],
+        [
+            [
+                "out1",
+                [
+                    "get",
+                    "x"
+                ]
+            ],
+            [
+                "set",
+                "z",
+                [
+                    "expr",
+                    [
+                        "get",
+                        "x"
+                    ],
+                    "+",
+                    [
+                        "get",
+                        "y"
+                    ]
+                ]
+            ],
+            [
+                "set",
+                "x",
+                [
+                    "get",
+                    "y"
+                ]
+            ],
+            [
+                "set",
+                "y",
+                [
+                    "get",
+                    "z"
+                ]
+            ]
+        ]
+    ]
+]
+let tree=test_tree2
+</script>
+
+<Tree {tree}/>
---
+++ src/Tree.svelte
@@ -0,0 +1,21 @@
+<script>
+export let tree
+export let initial=false
+$: opened=initial
+function toggle(event){ opened=!opened }
+</script>
+<main on:click|stopPropagation={toggle} 
+class="{ opened ? 'padding' : ''}" >
+[{#each tree as subtree}
+    {#if Array.isArray(subtree) }
+        <svelte:self tree={subtree} initial={false}/>
+    {:else}
+        {subtree}
+    {/if}
+    {#if opened}<br>{/if}
+{/each}]
+</main>
+<style>
+main { border: solid 1px black; display: inline-block; margin: 5px; }
+.padding { padding-left: 1cm; }
+</style>
